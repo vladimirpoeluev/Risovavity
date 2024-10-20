68c83ab79fba5542ee462b1909d3a78b33a1ebb1
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-10-20 11:08:50 +0300

    Обновление странци пользовательского приложения Avalonia

--- src/AvaloniaRisovaviti/ViewModel/ProfileEditerPageViewModel.cs
+++ src/AvaloniaRisovaviti/ViewModel/ProfileEditerPageViewModel.cs
@@ -1,19 +1,43 @@
+using Avalonia.Media;
+using AvaloniaRisovaviti.ProfileShows;
 using DomainModel.ResultsRequest;
 using InteractiveApiRisovaviti;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Security.Principal;
 
 namespace AvaloniaRisovaviti.ViewModel
 {
     internal class ProfileEditerPageViewModel: INotifyPropertyChanged
     {
         public UserResult UserResult { get; set; } = new UserResult();
+        public IImage Image { get; set; }
 
         public ProfileEditerPageViewModel()
         {
-            UserResult = new Profile(Authentication.AuthenticationUser.User).ProfileUser;
+
+			Image = new Avalonia.Media.Imaging.Bitmap("Accets/icoUser.png");
+
+			try
+            {
+				Profile profile = new Profile(Authentication.AuthenticationUser.User);
+				ProfileSetterImage setterImage = new ProfileSetterImage(profile);
+				UserResult = profile.ProfileUser;
+				Image = setterImage.UpdateImage();
+			}
+            catch (Exception)
+            {
+                UserResult = new UserResult() 
+                { 
+                    Name = "Пользователь не опознан",
+                    Email = "Данные неопознаны",
+                    IdRoleNavigation = new RoleResult() {  Name = "Роль не опознана"}
+                };
+
+            }
+
         }
         #region INotifyPropertyChanged Implementation
         public event PropertyChangedEventHandler? PropertyChanged;
