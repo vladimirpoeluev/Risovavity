5270d0d756ac604c64ec04cdf23772b7317b20ad
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-10-27 11:45:34 +0300

    Добавление интерфейса генирации хеша и верефикации пароля

--- src/Logic/HashPassword/GeneraterHash.cs
+++ src/Logic/HashPassword/GeneraterHash.cs
@@ -1,7 +1,17 @@
-﻿
+﻿using Logic.Interface;
+
 namespace Logic.HashPassword
 {
-    internal class GeneraterHash
-    {
-    }
+	internal class GeneraterHash : IGeneraterHash
+	{
+		public string Generate(string password)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Verify(string password)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
