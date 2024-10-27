c8aa9ae8ebda09b99e0d75a3cbee1353842b7588
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-10-27 15:09:08 +0300

    исправление бага с регистрацие она не работала из за слишком длинного хэша была увеличена область для неё

---
+++ src/DataIntegration/Migrations/20241027120256_PasswordEdit.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataIntegration.Migrations
+{
+    /// <inheritdoc />
+    public partial class PasswordEdit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "varchar(200)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldUnicode: false,
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)",
+                oldUnicode: false,
+                oldMaxLength: 50);
+        }
+    }
+}
