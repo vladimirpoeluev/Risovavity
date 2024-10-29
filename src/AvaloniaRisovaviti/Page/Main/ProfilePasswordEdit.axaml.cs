222c37315bbe039f2539bc220453a3bfa9b107b5
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-10-29 17:36:43 +0300

    Обновление пакетов и добавление окна изменения пароля

---
+++ src/AvaloniaRisovaviti/Page/Main/ProfilePasswordEdit.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaRisovaviti;
+
+public partial class ProfilePasswordEdit : UserControl
+{
+    public ProfilePasswordEdit()
+    {
+        InitializeComponent();
+    }
+}
