912bb8e3d9aabbb0a586aab2548a7e4bd6fd50a5
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-10-31 10:36:31 +0300

    Внедрение модели в пользовательский интерфейс

--- src/AvaloniaRisovaviti/Page/Main/ProfilePasswordEdit.axaml.cs
+++ src/AvaloniaRisovaviti/Page/Main/ProfilePasswordEdit.axaml.cs
@@ -1,13 +1,23 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using AvaloniaRisovaviti.ViewModel;
 
 namespace AvaloniaRisovaviti;
 
 public partial class ProfilePasswordEdit : UserControl
 {
+    ProfilePasswordEditViewModel _viewModel;
     public ProfilePasswordEdit()
     {
         InitializeComponent();
+        _viewModel = new ProfilePasswordEditViewModel();
+        DataContext = _viewModel;
     }
+
+    public void PasswordEdit_Click(object obj, RoutedEventArgs args)
+    {
+        _viewModel.PasswordEdit();
+    }
+
+
 }
