b3b51e9057a2be558a934050c225c73685b41ca1
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-11-03 14:54:07 +0300

    Реализация фабрики под все это дело(AutoControllerIntegraion)

---
+++ src/InteractiveApiRisovaviti/ControllerIntegration/CreaterAuthoControllersIntegraion.cs
@@ -0,0 +1,17 @@
+﻿using InteractiveApiRisovaviti.Interface;
+
+namespace InteractiveApiRisovaviti.ControllerIntegration
+{
+	internal class CreaterAuthoControllersIntegraion : FabricAutoControllerIntegraion
+	{
+		public override IGetAutoControllerIntegraion CreateGetPatser<T>(IAuthenticationUser user)
+		{
+			return new GetAutoControllerIntegraion(user);
+		}
+
+		public override IPostAutoControllerIntegraion CreatePostPatser<T>(IAuthenticationUser user, T obj)
+		{
+			return new PostAutoControllerIntegraion<T>(user, obj);
+		}
+	}
+}
---
+++ src/InteractiveApiRisovaviti/ControllerIntegration/FabricAutoControllerIntegraion.cs
@@ -0,0 +1,10 @@
+﻿using InteractiveApiRisovaviti.Interface;
+
+namespace InteractiveApiRisovaviti.ControllerIntegration
+{
+	internal abstract class FabricAutoControllerIntegraion
+	{
+		public abstract IPostAutoControllerIntegraion CreatePostPatser<T>(IAuthenticationUser user, T obj);
+		public abstract IGetAutoControllerIntegraion CreateGetPatser<T>(IAuthenticationUser user);
+	}
+}
