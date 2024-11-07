d5f42c2437870448820ce93f91e80c6eb83eb96d
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-11-07 17:59:51 +0300

    Добавление класса фабрики

---
+++ src/Logic/CanvasLogic/FabricCanvasOperation.cs
@@ -0,0 +1,25 @@
+﻿
+using DomainModel.Integration.Canvas;
+using DomainModel.ResultsRequest;
+using Logic.Interface;
+
+namespace Logic.CanvasLogic
+{
+	public class FabricCanvasOperation : IFabricCanvasOperation
+	{
+		public IAdderCanvas CreateAdderCanvas(UserResult user)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEditerCanvas CreateEditerCanvas(UserResult user)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IGetterCanvas CreateGetterCanvas(UserResult user)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
