4db2832c5c30c203dc938fdc4cc15ee4456c9126
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-11-12 09:46:52 +0300

    Создание моделей ImageResult и VeersionProjectResult

---
+++ src/DomainModel/ResultsRequest/Canvas/ImageResult.cs
@@ -0,0 +1,9 @@
+﻿
+namespace DomainModel.ResultsRequest.Canvas
+{
+	public class ImageResult
+	{
+		public string Name { get; set; }
+		public byte[] Image { get; set; }
+	}
+}
---
+++ src/DomainModel/ResultsRequest/Canvas/VersionProjectResult.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace DomainModel.ResultsRequest.Canvas
+{
+	public class VersionProjectResult
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int ParentVertionProject {  get; set; }
+		public int AuthorId { get; set; }
+	}
+}
