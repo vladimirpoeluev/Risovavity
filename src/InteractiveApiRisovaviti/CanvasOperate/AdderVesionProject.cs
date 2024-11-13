bfa153b5dbea9920556bf9429a2873ced31096e6
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-11-13 12:23:45 +0300

    Реализация добавления

---
+++ src/InteractiveApiRisovaviti/CanvasOperate/AdderVesionProject.cs
@@ -0,0 +1,31 @@
+﻿
+using DomainModel.Integration.CanvasOperation;
+using DomainModel.ResultsRequest.Canvas;
+using InteractiveApiRisovaviti.ControllerIntegration;
+using InteractiveApiRisovaviti.Interface;
+
+namespace InteractiveApiRisovaviti.CanvasOperate
+{
+	public class AdderVesionProject : IAdderVersionProject
+	{
+		FabricAutoControllerIntegraion _operates;
+		IAuthenticationUser _user;
+		public AdderVesionProject(IAuthenticationUser user)
+		{
+			_user = user;
+			_operates = new CreaterAuthoControllersIntegraion();
+		}
+
+		public Task AddVertionProjectAsync(VersionProjectForAddResult result)
+		{
+			var poster = _operates.CreatePostPatser(_user, result);
+			poster.ExecuteReques("api/VersionProject/add");
+			return Task.CompletedTask;
+		}
+
+		public Task DeleteVertionProjectAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
