00c75988fe55130cb9411edce26d62cacd099738
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-11-24 17:43:28 +0300

    Реализация асинхронного получения аватара пользователя

--- src/InteractiveApiRisovaviti/AvatarGetter.cs
+++ src/InteractiveApiRisovaviti/AvatarGetter.cs
@@ -1,16 +1,25 @@
-﻿using DomainModel.ResultsRequest;
+﻿using DomainModel.Integration;
+using DomainModel.ResultsRequest;
+using InteractiveApiRisovaviti.ControllerIntegration;
 using InteractiveApiRisovaviti.ControllerIntegration.AvatarUsers;
 using InteractiveApiRisovaviti.Interface;
 
 namespace InteractiveApiRisovaviti
 {
-	public class AvatarGetter
+	public class AvatarGetter : IUserAvatarGetter
 	{
 		IAvatarUsersControllerIntegration _getterAvatar;
+		IGetAutoControllerIntegraion _controllerIntegration;
 
 		public AvatarGetter(IAuthenticationUser user)
 		{
 			_getterAvatar = new AvatarUsersControllerIntegration(user);
+			_controllerIntegration = new GetAutoControllerIntegraion(user);
+		}
+
+		public async Task<UserAvatarResult> GetAvatarUserAsync(int id)
+		{
+			return await _controllerIntegration.GetResultAsync<UserAvatarResult>($"/api/AvatarUsers?id={id}");
 		}
 
 		public UserAvatarResult GetUserAvatar(int id)
