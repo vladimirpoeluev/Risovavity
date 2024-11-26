fd8c5ec151f63982ca92cc492c22e3ec891b085b
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-11-26 10:38:44 +0300

    Страница с информацией о хослта

---
+++ src/AvaloniaRisovaviti/Page/Main/CanvasInfoPage.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaRisovaviti;
+
+public partial class CanvasInfoPage : UserControl
+{
+    public CanvasInfoPage()
+    {
+        InitializeComponent();
+    }
+
+	private void ButtonAddVersion_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+	{
+        //TODO: Ќавигаци€ на страницу добавлени€ версии
+	}
+}
---
+++ src/AvaloniaRisovaviti/Page/Main/FormAddVersionPage.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaRisovaviti;
+
+public partial class FormAddVersionPage : UserControl
+{
+    public FormAddVersionPage()
+    {
+        InitializeComponent();
+    }
+}
