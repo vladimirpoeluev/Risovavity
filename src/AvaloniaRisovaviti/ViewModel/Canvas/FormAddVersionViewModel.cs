7d670a2a3945b867fa399a85a1f42edb1f1c7faf
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-12-01 11:29:42 +0300

    Подгрузка данных в модель

--- src/AvaloniaRisovaviti/ViewModel/Canvas/FormAddVersionViewModel.cs
+++ src/AvaloniaRisovaviti/ViewModel/Canvas/FormAddVersionViewModel.cs
@@ -1,6 +1,10 @@
 ﻿
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 using DomainModel.ResultsRequest.Canvas;
 using ReactiveUI.Fody.Helpers;
+using System.Threading.Tasks;
 
 namespace AvaloniaRisovaviti.ViewModel.Canvas
 {
@@ -9,13 +13,19 @@ namespace AvaloniaRisovaviti.ViewModel.Canvas
 		[Reactive]
 		public VersionProjectResult ProjectResult { get; set; }
 
+		[Reactive] public Task<IImage> ImageOldProjectReasult { get; set; }
+
 		[Reactive]
 		public VersionProjectResult NewProjectResult { get; set; }
+
+		[Reactive] public Task<IImage> NewImageProjectResult { get; set; }
 		
 		public FormAddVersionViewModel() 
 		{
 			ProjectResult = new VersionProjectResult();
 			NewProjectResult = new VersionProjectResult();
+			ImageOldProjectReasult = Task.FromResult((IImage)new Bitmap("\\Accets\\8.gif"));
+			NewImageProjectResult = Task.FromResult((IImage)new Bitmap(AssetLoader.Open(new System.Uri("avares:"))));
 		}
 
 		public FormAddVersionViewModel(VersionProjectResult parent) : this()
---
+++ src/AvaloniaRisovaviti/ViewModel/Fake/FakeFormAddVersionViewModel.cs
@@ -0,0 +1,22 @@
+﻿
+using AvaloniaRisovaviti.ViewModel.Canvas;
+
+namespace AvaloniaRisovaviti.ViewModel.Fake
+{
+	internal class FakeFormAddVersionViewModel : FormAddVersionViewModel
+	{
+		public FakeFormAddVersionViewModel() 
+		{
+			base.ProjectResult = new DomainModel.ResultsRequest.Canvas.VersionProjectResult()
+			{
+				Name = "Test",
+				Description = "Test",
+			};
+			base.NewProjectResult = new DomainModel.ResultsRequest.Canvas.VersionProjectResult() 
+			{
+				Name = "Test2",
+				Description = "Test2",
+			};
+		}
+	}
+}
