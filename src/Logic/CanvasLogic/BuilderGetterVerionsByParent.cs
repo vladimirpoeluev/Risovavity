8a77a41214840b163cce2151aaf0b7aff4d3e5c3
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-12-04 11:33:04 +0300

    Реализация построителя экземпляра получения версии версий

---
+++ src/Logic/CanvasLogic/BuilderGetterVerionsByParent.cs
@@ -0,0 +1,30 @@
+﻿
+using DataIntegration.Model;
+using DomainModel.Integration.CanvasOperation;
+using Logic.CanvasLogic.VersionProjectOperate;
+
+namespace Logic.CanvasLogic
+{
+	public class BuilderGetterVerionsByParent
+	{
+		private GetterVersionsByParent _getterVesionsByParent;
+		public BuilderGetterVerionsByParent(DatabaseContext databaseContext) 
+		{
+			_getterVesionsByParent = new GetterVersionsByParent(databaseContext);
+		}
+
+		public BuilderGetterVerionsByParent Skip(int skip)
+		{
+			_getterVesionsByParent.Skip = skip;
+			return this;
+		}
+
+		public BuilderGetterVerionsByParent Take(int take)
+		{
+			_getterVesionsByParent.Take = take;
+			return this;
+		}
+
+		public IGetterVersionByParentVersion ToGetter() => _getterVesionsByParent;
+	}
+}
