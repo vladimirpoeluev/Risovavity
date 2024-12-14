18a9bec5d2b22136fbc17114bc6932c58757d756
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-12-14 19:19:38 +0300

    Создание сервиса авторизации при помощи токена восстановления и токена доступа

---
+++ src/RisovavitiApi/JwtBearerAuthentication/AuthorizeServiceRefresh.cs
@@ -0,0 +1,23 @@
+﻿using DomainModel.ResultsRequest;
+using RisovavitiApi.JwtBearerAuthentication.Interface;
+using RisovavitiApi.Model;
+
+namespace RisovavitiApi.JwtBearerAuthentication
+{
+	public class AuthorizeServiceRefresh : IAutorizeServiceRefresh
+	{
+
+		public AuthorizeServiceRefresh()
+		{ }
+
+		public TokensRefreshAndAccess ExtendSession(string refresh)
+		{
+			throw new NotImplementedException();
+		}
+
+		public TokensRefreshAndAccess RegistSession(UserResult user)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/RisovavitiApi/JwtBearerAuthentication/Interface/IAutorizeServiceRefresh.cs
@@ -0,0 +1,11 @@
+﻿using DomainModel.ResultsRequest;
+using RisovavitiApi.Model;
+
+namespace RisovavitiApi.JwtBearerAuthentication.Interface
+{
+	public interface IAutorizeServiceRefresh 
+	{
+		TokensRefreshAndAccess ExtendSession(string refresh);
+		TokensRefreshAndAccess RegistSession(UserResult user);
+	}
+}
