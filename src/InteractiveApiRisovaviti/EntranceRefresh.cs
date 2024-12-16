33d55273f895b7d5c5b28d551e68dc985fe4f1da
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-12-16 16:52:10 +0300

    Добавление новой реализации входа

---
+++ src/InteractiveApiRisovaviti/EntranceRefresh.cs
@@ -0,0 +1,17 @@
+﻿
+using InteractiveApiRisovaviti.Interface;
+
+namespace InteractiveApiRisovaviti
+{
+	public class EntranceRefresh : IEntrance
+	{
+		public EntranceRefresh()
+		{
+
+		}
+		IAuthenticationUser IEntrance.IputSystem(string login, string password)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
