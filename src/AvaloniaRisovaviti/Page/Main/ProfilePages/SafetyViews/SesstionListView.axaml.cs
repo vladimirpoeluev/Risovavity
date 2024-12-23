acc75a0debd47ccef9a0ffb5af59009adcb49e71
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-12-23 13:50:50 +0300

    Создание страниц сессий

---
+++ src/AvaloniaRisovaviti/Page/Main/ProfilePages/SafetyViews/SesstionListView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaRisovaviti;
+
+public partial class SesstionListView : UserControl
+{
+    public SesstionListView()
+    {
+        InitializeComponent();
+    }
+}
