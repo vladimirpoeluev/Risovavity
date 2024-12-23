7c12e23211918bbba2f66b3ec4762e1a6b057ebf
vladimirpoeluev <poeluev.vladimir@gmail.com>
2024-12-23 13:36:15 +0300

    Страница безопасности

---
+++ src/AvaloniaRisovaviti/Page/Main/ProfilePages/SafetyView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaRisovaviti;
+
+public partial class SafetyView : UserControl
+{
+    public SafetyView()
+    {
+        InitializeComponent();
+    }
+}
