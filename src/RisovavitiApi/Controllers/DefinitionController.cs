6e1e3663637846ec5d63455ff2a5ae5c121ec5bf
vladimirpoeluev <poeluev.vladimir@gmail.com>
2025-01-08 21:46:36 +0300

    Создание контроллера который предоставляет информацию о правах пользоватлея

---
+++ src/RisovavitiApi/Controllers/DefinitionController.cs
@@ -0,0 +1,41 @@
+﻿using DomainModel.Integration.CanvasOperation;
+using DomainModel.ResultsRequest.Canvas;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RisovavitiApi.Model.Interfaces;
+
+namespace RisovavitiApi.Controllers
+{
+	[Authorize]
+	[ApiController]
+	[Route("api/permission")]
+	public class DefinitionController : Controller
+	{
+		IDefinitionerOfPermissionByHttpContext _definitionerOfPermission;
+		IGetterCanvas _getterCanvas;
+		IGetterVersionProject _getterVersion;
+		
+		public DefinitionController(IDefinitionerOfPermissionByHttpContext definitionerOfPermission,
+									IGetterCanvas getterCanvas,
+									IGetterVersionProject getterVersion) 
+		{
+			_definitionerOfPermission = definitionerOfPermission;
+			_getterCanvas = getterCanvas;
+			_getterVersion = getterVersion;
+		}
+
+		[HttpGet("canvas/{canvasId}")]
+		public async Task<IActionResult> GetPermissionCanvas(int canvasId)
+		{
+			CanvasResult canvasResult = await _getterCanvas.GetAsync(canvasId);
+			return Ok(_definitionerOfPermission.GetPermissionResult(canvasResult));
+		}
+
+		[HttpGet("version-project/{versionId}")]
+		public async Task<IActionResult> GetPermissionVersionProject(int versionId)
+		{
+			VersionProjectResult versionProject = await _getterVersion.GetVersionProjectByIdAsync(versionId);
+			return Ok(_definitionerOfPermission.GetPermissionResult(versionProject));
+		}
+	}
+}
