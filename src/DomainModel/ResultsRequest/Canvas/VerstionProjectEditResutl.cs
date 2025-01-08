b3b0430960822207d9d9f2a7d8cdf0da15378a58
vladimirpoeluev <poeluev.vladimir@gmail.com>
2025-01-08 11:53:28 +0300

    Добавление модели для редактирвания имени и описания версии

---
+++ src/DomainModel/ResultsRequest/Canvas/VerstionProjectEditResutl.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DomainModel.ResultsRequest.Canvas
+{
+	public class VerstionProjectEditResutl
+	{
+		public int VerstionId { get; set; }
+		public string NameEdit { get; set; } = string.Empty;
+		public string DescriptionEdit { get; set; } = string.Empty;
+	}
+}
