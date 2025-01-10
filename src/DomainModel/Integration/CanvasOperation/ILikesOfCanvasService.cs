1da526d127d8f7be4ac46ea79fa42c31465e0ea5
vladimirpoeluev <poeluev.vladimir@gmail.com>
2025-01-10 11:54:54 +0300

    Создание интерфейса для операций над лайками

---
+++ src/DomainModel/Integration/CanvasOperation/ILikesOfCanvasService.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace DomainModel.Integration.CanvasOperation
+{
+	public interface ILikesOfCanvasService
+	{
+		Task Like(int canvasId);
+		Task UnLike(int canvasId);
+		Task<int> CouintLikes(int canvasId);
+		Task<bool> IsLike(int canvasId);
+	}
+}
