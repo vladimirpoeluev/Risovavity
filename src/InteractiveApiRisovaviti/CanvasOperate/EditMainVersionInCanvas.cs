7a643acdf6c5d346afb4d57083720bf07b400197
vladimirpoeluev <poeluev.vladimir@gmail.com>
2025-01-15 10:36:50 +0300

    Еще парсеры

---
+++ src/InteractiveApiRisovaviti/CanvasOperate/EditMainVersionInCanvas.cs
@@ -0,0 +1,24 @@
+﻿
+using DomainModel.Integration.CanvasOperation;
+using DomainModel.ResultsRequest.Canvas;
+using InteractiveApiRisovaviti.ControllerIntegration;
+using InteractiveApiRisovaviti.Interface;
+
+namespace InteractiveApiRisovaviti.CanvasOperate
+{
+	public class EditMainVersionInCanvas : IEditMainVerstionInCanvas
+	{
+		IAuthenticationUser User { get; set; }
+		FabricAutoControllerIntegraion _fabricAuto;
+		public EditMainVersionInCanvas(	IAuthenticationUser user, 
+										FabricAutoControllerIntegraion fabricAuto) 
+		{
+			User = user;
+			_fabricAuto = fabricAuto;
+		}
+		public async Task SelectMainVerstion(MainVersionInCanvasResutl mainVersion)
+		{
+			await _fabricAuto.CreatePostPatser(User, mainVersion).ExecuteRequestAsync("api/canvas/edit/mainVersion");
+		}
+	}
+}
---
+++ src/InteractiveApiRisovaviti/CanvasOperate/EditVersionProject.cs
@@ -0,0 +1,24 @@
+﻿
+using DomainModel.Integration.CanvasOperation;
+using DomainModel.ResultsRequest.Canvas;
+using InteractiveApiRisovaviti.ControllerIntegration;
+using InteractiveApiRisovaviti.Interface;
+
+namespace InteractiveApiRisovaviti.CanvasOperate
+{
+	public class EditVersionProject : IEditVersionProject
+	{
+		FabricAutoControllerIntegraion _fabricAuto;
+		IAuthenticationUser User { get; set; }
+		public EditVersionProject(	FabricAutoControllerIntegraion fabricAuto,
+									IAuthenticationUser user) 
+		{
+			_fabricAuto = fabricAuto;
+			User = user;
+		}
+		public async Task Edit(VerstionProjectEditResutl newVersionProject)
+		{
+			await _fabricAuto.CreatePostPatser(User, newVersionProject).ExecuteRequestAsync("api/VersionProject/edit");
+		}
+	}
+}
