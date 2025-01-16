200c8720945ce4964c062a97eea7247d20d25a19
vladimirpoeluev <poeluev.vladimir@gmail.com>
2025-01-16 10:52:56 +0300

    Определение прав пользователя

---
+++ src/InteractiveApiRisovaviti/CanvasOperate/DefinitionerOfPermission.cs
@@ -0,0 +1,30 @@
+﻿
+using DomainModel.ResultsRequest;
+using DomainModel.ResultsRequest.Canvas;
+using InteractiveApiRisovaviti.ControllerIntegration;
+using InteractiveApiRisovaviti.Interface;
+
+namespace InteractiveApiRisovaviti.CanvasOperate
+{
+    public class DefinitionerOfPermission : IDefinitionerOfPermission
+	{
+		IAuthenticationUser User { get; set; }
+		FabricAutoControllerIntegraion _fabricAuto;
+		public DefinitionerOfPermission(IAuthenticationUser user,
+										FabricAutoControllerIntegraion fabricAuto)
+		{
+			User = user;
+			_fabricAuto = fabricAuto;
+		}
+
+		public async Task<PermissionResult> GetPermissionAsync(CanvasResult canvas)
+		{
+			return await _fabricAuto.CreateGetPatser(User).GetResultAsync<PermissionResult>($"api/permission/canvas/{canvas.Id}");
+		}
+
+		public async Task<PermissionResult> GetPermissionAsync(VersionProjectResult versionProject)
+		{
+			return await _fabricAuto.CreateGetPatser(User).GetResultAsync<PermissionResult>($"api/permission/version-project/{versionProject.Id}");
+		}
+	}
+}
---
+++ src/InteractiveApiRisovaviti/Interface/IDefinitionerOfPermission.cs
@@ -0,0 +1,11 @@
+﻿using DomainModel.ResultsRequest;
+using DomainModel.ResultsRequest.Canvas;
+
+namespace InteractiveApiRisovaviti.Interface
+{
+    public interface IDefinitionerOfPermission
+    {
+        Task<PermissionResult> GetPermissionAsync(CanvasResult canvas);
+        Task<PermissionResult> GetPermissionAsync(VersionProjectResult versionProject);
+    }
+}
