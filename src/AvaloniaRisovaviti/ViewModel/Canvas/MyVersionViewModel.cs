8715c3e2d730dec4075ca01d566bdf16ce6ec913
vladimirpoeluev <poeluev.vladimir@gmail.com>
2025-03-13 11:14:11 +0300

    Начало реализации модели со своимои версиями

--- src/AvaloniaRisovaviti/ViewModel/Canvas/MyVersionViewModel.cs
+++ src/AvaloniaRisovaviti/ViewModel/Canvas/MyVersionViewModel.cs
@@ -1,10 +1,31 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using AvaloniaRisovaviti.Model;
+using DomainModel.Integration;
+using InteractiveApiRisovaviti.Interface;
 using ReactiveUI;
 
 namespace AvaloniaRisovaviti.ViewModel.Canvas
 {
 	public class MyVersionViewModel : ReactiveObject
 	{
+		private IGetterWorkByAuthorId _getter;
+		private InteractiveApiRisovaviti.Profile _profile;
+		public IEnumerable<VersionProjectResultWithImage> VersionsProject { get; set; }
+
+		public MyVersionViewModel(IGetterWorkByAuthorId getter, IAuthenticationUser user) 
+		{
+			_getter = getter;
+			_profile = new InteractiveApiRisovaviti.Profile(user);
+			VersionsProject = new List<VersionProjectResultWithImage>();
+			Task.WaitAll(InitVersionsProject());
+		}
+		
+		private async Task InitVersionsProject()
+		{
+
+		}
+
 	}
 }
