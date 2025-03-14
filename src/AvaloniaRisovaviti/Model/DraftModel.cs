fc9a7cabf7c1035e7d86097cd2e464128e8c2f93
vladimirpoeluev <poeluev.vladimir@gmail.com>
2025-03-14 14:22:57 +0300

    Реализация создания проекта в черновиках

---
+++ src/AvaloniaRisovaviti/Model/DraftModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System;
+
+namespace AvaloniaRisovaviti.Model
+{
+	internal class DraftModel
+	{
+		public Guid Guid { get; set; }
+		public DraftInfo DraftInfo { get; set; }
+		public byte[] Images { get; set; }
+	}
+}
---
+++ src/AvaloniaRisovaviti/Services/AdderNewProject.cs
@@ -0,0 +1,41 @@
+﻿using AvaloniaRisovaviti.Model;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AvaloniaRisovaviti.Services
+{
+    internal class AdderNewProject
+    {
+        public static string Path = "projects";
+        public AdderNewProject() { }
+
+        public async Task<Guid> AddProject(DraftModel draft)
+        {
+            var path = Path + $"/{draft.Guid}";
+			AddDirect(path);
+            using (FileStream writer = new FileStream(path + "/image", FileMode.OpenOrCreate))
+            {
+                await writer.WriteAsync(draft.Images);
+            }
+
+            using StreamWriter writerJson = new StreamWriter(path + "/info.json");
+            await writerJson.WriteAsync(ObjectConvert(draft.DraftInfo));
+
+            return draft.Guid;
+        }
+
+        private void AddDirect(string path)
+        {
+			if (!Directory.Exists(path))
+			    Directory.CreateDirectory(path);
+		}
+
+        private string ObjectConvert(DraftInfo obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+    }
+}
