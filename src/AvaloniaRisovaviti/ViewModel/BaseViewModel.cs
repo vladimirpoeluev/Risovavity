2fc06f6e3bd9243f548d0a0a7668578862931427
vladimirpoeluev <poeluev.vladimir@gmail.com>
2025-04-09 16:39:21 +0300

    Обработка ошибок в базовом представлении

--- src/AvaloniaRisovaviti/ViewModel/BaseViewModel.cs
+++ src/AvaloniaRisovaviti/ViewModel/BaseViewModel.cs
@@ -1,4 +1,7 @@
+using Avalonia.Controls;
+using InteractiveApiRisovaviti.Exceptions;
 using ReactiveUI;
+using System;
 using System.Reactive;
 
 namespace AvaloniaRisovaviti.ViewModel
@@ -6,14 +9,46 @@ namespace AvaloniaRisovaviti.ViewModel
 	public class BaseViewModel : ReactiveObject, IActivatableView
 	{
 		public ReactiveCommand<Unit, Unit> LoadCommand { get; set; }
+		public ReactiveCommand<UserControl, UserControl> Error { get; set; }
 		public BaseViewModel() 
 		{
 			LoadCommand = ReactiveCommand.Create(Load);
+			Error = ReactiveCommand.Create<UserControl, UserControl>(ErrorMetod);
 		}
 
-		public virtual void Load() 
+		public virtual void Load() {}
+
+		public UserControl ErrorMetod(UserControl errorView) => errorView;
+
+		public void TryAction(Action action)
 		{
-			
+			try
+			{
+				action();
+			}
+			catch (AuthorizeException ex)
+			{
+				Error.Execute(new ErrorAuthorizeView());
+			}
+			catch(RequstException ex)
+			{
+				if(ex.ErrorMessageRequest.NumberError == 404)
+				{
+					Error.Execute(new Error404View());
+				}
+				else if(ex.ErrorMessageRequest.NumberError == 500)
+				{
+					Error.Execute(new Error500View());
+				}
+				else
+				{
+					Error.Execute(new Error500View());
+				}
+			}
+			catch(Exception ex)
+			{
+				Error.Execute(new ErrorNotConnectView());
+			}
 		}
 	}
 }
